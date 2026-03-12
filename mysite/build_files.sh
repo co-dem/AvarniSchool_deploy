77ebb0b531e157696426c32aafbf73fd0202b86c
codem <96345940+co-dem@users.noreply.github.com>
2026-03-13 01:09:13 +0300

    Update build_files.sh

--- mysite/build_files.sh
+++ mysite/build_files.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-
-echo "BUILD START"
-
-# Установка зависимостей
-python3.9 -m pip install -r requirements.txt
-
-# Сбор статических файлов
-python3.9 manage.py collectstatic --noinput --clear
-
-echo "BUILD END"
+set -e
+cd mysite
+pip install -r requirements.txt
+python manage.py collectstatic --noinput
