1d4a82e6520e1d823e2eea53690f6ea936bf5723
codem <96345940+co-dem@users.noreply.github.com>
2026-03-17 22:05:57 +0300

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -7,7 +7,7 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-test-key-for-vercel-12345'
+SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = False
 
 # ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
