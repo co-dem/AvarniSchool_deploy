20d4ddc75358f8ff013e0e43d9ae23c8fcadc836
codem <96345940+co-dem@users.noreply.github.com>
2026-03-17 21:25:20 +0300

    Delete vercel-build.sh

--- vercel-build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Установка зависимостей через pip..."
-pip install --no-cache-dir -r mysite/requirements.txt
-
-echo "Сбор статических файлов..."
-cd mysite
-python manage.py collectstatic --noinput
